746bdcd07b5665c33e8c40a1c2585d5ab736cba9
Nikko Ortega <NFCOrtega89@gmail.com>
2017-05-04 21:58:14 -0700

    get

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   const API_KEY = '4195135e9c7034878d78c6f4d7f1f88d'
   const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
-  const lat = req.body.lat
-  const lon = req.body.lon
+  const lat = req.params.lat
+  const lon = req.params.lon
   axios.get(`${ROOT_URL}&lat=${lat}&lon=${lon}`)
     .then(response => {res.send(response.data)})
 })
