1894e9d29ed9ecf167ee99eee10629e851356061
Nikko Ortega <NFCOrtega89@gmail.com>
2017-05-04 22:14:29 -0700

    hello

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  const API_KEY = '4195135e9c7034878d78c6f4d7f1f88d'
-  const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
-  const lat = req.query.lat
-  const lon = req.query.lon
-  axios.get(`${ROOT_URL}&lat=${lat}&lon=${lon}`)
-    .then(response => {res.json(response.data)})
-    .catch(err => res.status(400).send(err))
+  res.send('hello')
+  // const API_KEY = '4195135e9c7034878d78c6f4d7f1f88d'
+  // const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
+  // const lat = req.query.lat
+  // const lon = req.query.lon
+  // axios.get(`${ROOT_URL}&lat=${lat}&lon=${lon}`)
+  //   .then(response => {res.json(response.data)})
+  //   .catch(err => res.status(400).send(err))
 })
 
 
